9902cdca8282197ae2ad81f8e7005c4584bb9760
Katiely <katiely.eu@gmail.com>
2019-06-30 20:40:25 -0400

    :bacon: [ADD] Exercicio 6 :canoe:

---
+++ tRexx/api/models/Curso.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    attributes: {
+        nome:{
+            type:'string',
+            required:true,
+            maxLength:64
+
+          
+        },
+        sigla:{
+            type:'string',
+            required:true,
+            maxLength:4
+
+
+        },
+
+        descricao:{
+            type:'string'
+
+        },
+
+    },
+}
---
+++ tRexx/api/models/Jogada.js
@@ -0,0 +1,22 @@
+module.exports = {
+    attributes: {
+        jogador: {
+            type: 'number',
+            required:true,
+          },
+          data:{
+
+            type:'string',
+            required:true,
+            maxLength:45
+
+
+          },
+
+          pontuacao:{
+            type:'number',
+            required:true
+          },
+    },
+
+}
